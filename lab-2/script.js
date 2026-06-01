61b7ce664da0bc8100ee74507e7248b91eec64ee
kayo1122 <Romanomorra@outlook.com>
2026-06-01 14:32:06 -0400

    if/elseif/else statements

--- lab-2/script.js
+++ lab-2/script.js
@@ -11,5 +11,23 @@ let output = document.querySelector("#output");
 //output message
 let message = "";
 
-
+// if / else if / else statements
+if (health < 30 && enemyNearby === true) {
+    message = "CRITICAL ALERT: Immediate Evacuation Required";
+}
+else if (ammo < 5 || shield === false) {
+    message = "Warning: Low Resources";
+}
+else if (missionProgress >= 1 && missionProgress <= 70) {
+    message = "Mission In Progress";
+}
+else if (missionProgress > 70 && enemyNearby === false) {
+    message = "Approaching Mission Completion";
+}
+else if (missionProgress === 100) {
+    message = "Mission Complete Successfully";
+}
+else {
+    message = "System Stable";
+}
 
