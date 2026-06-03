42b597f7777685f1107a4d749260d1e977c4b34b
kayo1122 <Romanomorra@outlook.com>
2026-06-03 16:44:16 -0400

    cleaned up code

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -12,8 +12,37 @@ let weaponLoadoutIndex = 0;
 let missionObjectiveIndex = 0;
 let riskLevelIndex = 0;
 
-//cycle through options
+//cycle through options function
 function cycleOptions(array, currentIndex) {
     let nextIndex = (currentIndex + 1) % array.length;
     return nextIndex;
 }
+
+//cycle through for each category
+function cycleAgentType() {
+    agentTypeIndex = cycleOptions(agentType, agentTypeIndex);
+    document.getElementById("agentType").textContent = agentType[agentTypeIndex];
+}
+function cycleMissionLocation() {
+    missionLocationIndex = cycleOptions(missionLocation, missionLocationIndex);
+    document.getElementById("missionLocation").textContent = missionLocation[missionLocationIndex];
+}
+function cycleWeaponLoadout() {
+    weaponLoadoutIndex = cycleOptions(weaponLoadout, weaponLoadoutIndex);
+    document.getElementById("weaponLoadout").textContent = weaponLoadout[weaponLoadoutIndex];
+}
+function cycleMissionObjective() {
+    missionObjectiveIndex = cycleOptions(missionObjective, missionObjectiveIndex);
+    document.getElementById("missionObjective").textContent = missionObjective[missionObjectiveIndex];
+}
+function cycleRiskLevel() {
+    riskLevelIndex = cycleOptions(riskLevel, riskLevelIndex);
+    document.getElementById("riskLevel").textContent = riskLevel[riskLevelIndex];
+}
+
+//event listeners
+document.getElementById("btn-agent").addEventListener("click", cycleAgentType);
+document.getElementById("btn-location").addEventListener("click", cycleMissionLocation);
+document.getElementById("btn-weapon").addEventListener("click", cycleWeaponLoadout);
+document.getElementById("btn-objective").addEventListener("click", cycleMissionObjective);
+document.getElementById("btn-risk").addEventListener("click", cycleRiskLevel);
