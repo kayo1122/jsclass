65b75abb29cb8ed906d72c4126ff25fe37587c4f
kayo1122 <Romanomorra@outlook.com>
2026-07-29 15:27:21 -0400

    search movies function complete added student ID and Name to JS

--- Assignment2/js/script.js
+++ Assignment2/js/script.js
@@ -8,8 +8,13 @@ const results =
     document.querySelector("#results");
 const studentInfo = 
     document.querySelector("#studentInfo");
-const apiKey = "";
+const apiKey = "af9becee128197eb41adfa048a2e40e7";
 
+const studentName = "Romano Scerbo-Morra";
+const studentId = "200673567";
+
+studentInfo.textContent =
+    `Student: ${studentName} | Student ID: ${studentId}`;
 
 searchForm.addEventListener("submit", function(event){
     event.preventDefault();
@@ -24,7 +29,24 @@ searchForm.addEventListener("submit", function(event){
     searchMovies(SearchTerm);
 });
 
-async function searchMovies(SearchTerm) {
-
-}
+    async function searchMovies(SearchTerm) {
+        const encodedSearchTerm = encodeURIComponent(SearchTerm);
+        const apiUrl =
+            `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodedSearchTerm}&include_adult=false&language=en-US&page=1`;
+        try {
+            statusMessage.textContent = `loading movies...`;
+            results.replaceChildren();
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(data);
+            console.log(data.results);
+        }        
+        catch(error){
+            console.error("Error fetching movie data:", error);
+            statusMessage.textContent = "An error occurred while fetching movie data.";
+        }
+    }
 
